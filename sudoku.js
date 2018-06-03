5251b29b1d3eec7144dbc9eaf337353da13ead2d
unknown <brian.pradipta.a@gmail.com>
2018-06-03 15:10:21 +0700

    brian - full released

--- sudoku.js
+++ sudoku.js
@@ -1,12 +1,13 @@
 "use strict"
 
 class Sudoku {
-  constructor(board_string) {
-    this.str = board_string;
-    this.fullBoard = this.strToArr(board_string);
+  constructor(boardString) {
+    this.str = boardString;
+    this.fullBoard = this.numArr(boardString);
+    this.emptyPositions = this.emptyPositions(this.fullBoard);
   }
 
-  strToArr (str){
+  numArr (str){
     let mainArr = [];
     let rowArr = [];
     for (let i in str){
@@ -19,17 +20,111 @@ class Sudoku {
     return mainArr
   }
 
-  solve() {}
+  emptyPositions (fullBoard){
+    var emptyPositions = [];
+    for (let i in fullBoard){
+      for (let j in fullBoard[i]){
+        if (fullBoard[i][j] === 0){
+          emptyPositions.push([+i, +j]);
+        }
+      }
+    }
+    return emptyPositions;
+  }
+
+  checkRow (fullBoard, row, num){
+    for (let i in fullBoard[row]){
+      if (fullBoard[row][i] === num){
+        return false;
+      }
+    }
+    return true;
+  }
+
+  checkCol (fullBoard, col, num){
+    for (let i in fullBoard){
+      if (fullBoard[i][col] === num){
+        return false;
+      }
+    }
+    return true;
+  }
+
+  checkBox (fullBoard, row, col, num){
+    // nentuin titik2 batasan kotak
+    var colPojok = 0;
+    var rowPojok = 0;
+    var boxSize = 3;
+
+    // bikin titik2 batasan yg baru
+    while (row >= rowPojok + boxSize){
+      rowPojok += boxSize;
+    }
+    while (col >= colPojok + boxSize){
+      colPojok += boxSize;
+    }
+
+    for (let i = rowPojok; i < rowPojok + boxSize; i++){
+      for (let j = colPojok; j < colPojok + boxSize; j++){
+        if(fullBoard[i][j] === num){
+          return false;
+        }
+      }
+    }
+    return true;
+  }
+
+  checkAll (fullBoard, row, col, num){
+    if (this.checkRow(fullBoard, row, num) && 
+    this.checkCol(fullBoard, col, num) && 
+    this.checkBox(fullBoard, row, col, num)){
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
+
+  solve() {
+    var limit = 9;
+
+    for(let i = 0; i < this.emptyPositions.length;){
+      let row = this.emptyPositions[i][0];
+      let col = this.emptyPositions[i][1];
+      let isFound = false;
+      // increment num2 yg mungkin dapat jadi solusi
+      let num = this.fullBoard[row][col] + 1;
+      // coba num sampe num mencapai limit (9) atau solusi dianggap ketemu (found)
+      while(isFound === false && num <= limit){
+        // kalo num dites cocok, anggap solusi ketemu(found), assign nilainya, lanjut next ke kotak kosong selanjutnya
+        if(this.checkAll(this.fullBoard, row, col, num)){
+          isFound = true;
+          this.fullBoard[row][col] = num;
+          i++;
+        }
+        // kalo ngga, coba num berikutnya
+        else {
+          num++;
+        }
+      }
+      // kalo num2 dalam limit yg ditentuin (9) gbs jd solusi, kosongin lg nilai di kotak saat ini, dan balik ke kotak kosong sebelumnya; krn berarti num yg sebelumnya diassign blm jd solusi yg tepat
+      if (isFound === false){
+        this.fullBoard[row][col] = 0;
+        i--;
+      }
+    }
+    return this.board(this.fullBoard);
+  }
 
   // Returns a string representing the current state of the board
-  board() {
+  board(fullBoard) {
     var mainBoard = '------------';
     var allRow = '';
     var rowInBox = '';
     var mainBoardRow = 0;
-    for (let i in this.fullBoard){
-      for (let j in this.fullBoard[i]){
-        rowInBox += this.fullBoard[i][j];
+    for (let i in fullBoard){
+      for (let j in fullBoard[i]){
+        rowInBox += fullBoard[i][j];
         if (allRow.length >= 8){
           allRow += rowInBox;
           rowInBox = '';
@@ -48,7 +143,6 @@ class Sudoku {
         mainBoardRow = 0;
       }
     }
-
    return mainBoard
   }
 }
@@ -57,14 +151,14 @@ class Sudoku {
 // The file has newlines at the end of each line,
 // so we call split to remove it (\n)
 var fs = require('fs')
-var board_string = fs.readFileSync('set-01_sample.unsolved.txt')
+var boardString = fs.readFileSync('set-01_sample.unsolved.txt')
   .toString()
   .split("\n")[0]
 
-var game = new Sudoku(board_string)
+var game = new Sudoku(boardString)
 
 // Remember: this will just fill out what it can and not "guess"
-game.solve()
+console.log(game.solve())
 
-console.log(game.board())
-// console.log(game.fullBoard)
+// console.log(game.fullboard)
+
